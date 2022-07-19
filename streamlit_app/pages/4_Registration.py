5b4b6fc3c02a3102f497411e01c1c72be6f9d458
mariashev <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>
2022-07-19 15:42:12 +0000

    Upload image feature update

--- streamlit_app/pages/4_Registration.py
+++ streamlit_app/pages/4_Registration.py
@@ -1,4 +1,16 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.markdown("# Registration")
 st.sidebar.markdown("# Registration")
+
+imtype = option_menu("Select Image Type", ["2D Registration", "3D Registration"], menu_icon="image",default_index=0)
+if imtype == "2D Registration": 
+    file = st.file_uploader('Upload An Image')
+    st.write("For testing purposes, you can select a sample image here")
+    st.button("Rerun")
+
+if imtype == "3D Registration":
+    file = st.file_uploader('Upload An Image')
+    st.write("For testing purposes, you can select a sample image here")
+    st.button("Rerun")
--- streamlit_app/pages/5_Segmentation.py
+++ streamlit_app/pages/5_Segmentation.py
@@ -1,4 +1,16 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
 st.markdown("# Segmentaion")
 st.sidebar.markdown("# Segmentation")
+
+imtype = option_menu("Select Image Type", ["2D Segmentation", "3D Segmentation"], menu_icon="image",default_index=0)
+if imtype == "2D Segmentation": 
+    file = st.file_uploader('Upload An Image')
+    st.write("For testing purposes, you can select a sample image here")
+    st.button("Rerun")
+
+if imtype == "3D Segmentation":
+    file = st.file_uploader('Upload An Image')
+    st.write("For testing purposes, you can select a sample image here")
+    st.button("Rerun")

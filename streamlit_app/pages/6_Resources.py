9cf100412036858ea859eb3a0ac5cc59f2787460
mariashev <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>
2022-07-19 15:37:31 +0000

    Update Resources Page

--- streamlit_app/pages/6_Resources.py
+++ streamlit_app/pages/6_Resources.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.markdown("# Resources")
 st.sidebar.markdown("# Resources")
+
+st.write("To contribute or learn more about our open-source project, find us on [GitHub](https://github.com/rh-aiservices-bu/rhods_monai)")
+st.write("**Additional Resources**")
+st.write("1. [MONAI Documentation](https://github.com/Project-MONAI/MONAI)")
+st.write("2. [Streamlit Documentation](https://docs.streamlit.io/)")
+st.write("3. [Using RHODS with GPUs Tutorial](https://docs.google.com/presentation/d/1djPRIfaCmX_u5Bvw_qr3dUkO6r-gGXlQ8Df6SUbb8sk/edit?pli=1#slide=id.p)")
+st.write("4. [MedMNIST Documentation](https://github.com/MedMNIST/MedMNIST)")

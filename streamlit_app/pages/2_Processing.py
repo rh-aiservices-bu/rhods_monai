8aa0cd10caf467a02192855e06120a8854b43bc9
Maria <maria410@bu.edu>
2022-08-01 17:03:25 +0000

    edit page format

--- streamlit_app/pages/2_Processing.py
+++ streamlit_app/pages/2_Processing.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.markdown("# Image Processing")
 st.sidebar.markdown("# Image Processing")
@@ -6,6 +7,6 @@ st.sidebar.markdown("# Image Processing")
 file = st.file_uploader('Upload An Image')
 st.write("For testing purposes, you can select a sample image [here](https://www.kaggle.com/datasets/andrewmvd/medical-mnist)")
 if file:
-    img = PILImage.open(file)
+    img = Image.open(file)
     st.image(img, use_column_width=True)
     st.button("Click Here to Process")
--- streamlit_app/pages/4_Registration.py
+++ streamlit_app/pages/4_Registration.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from PIL import Image
 
 st.markdown("# Registration")
 st.sidebar.markdown("# Registration")
@@ -8,9 +9,15 @@ imtype = option_menu("Select Image Type", ["2D Registration", "3D Registration"]
 if imtype == "2D Registration": 
     file = st.file_uploader('Upload An Image')
     st.write("For testing purposes, you can select a sample image [here](https://www.kaggle.com/datasets/andrewmvd/medical-mnist)")
-    st.button("Rerun")
+    if file:
+        img = Image.open(file)
+        st.image(img, use_column_width=True)
+        st.button("Click Here to Register")
 
 if imtype == "3D Registration":
     file = st.file_uploader('Upload An Image')
     st.write("For testing purposes, you can select a sample image [here](https://www.kaggle.com/datasets/andrewmvd/medical-mnist)")
-    st.button("Rerun")
+    if file:
+        img = Image.open(file)
+        st.image(img, use_column_width=True)
+        st.button("Click Here to Register")

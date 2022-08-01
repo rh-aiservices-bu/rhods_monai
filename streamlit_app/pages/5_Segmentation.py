5f2679cd28aa1e8d91f483f7a7ceabbd168212f7
Maria <maria410@bu.edu>
2022-08-01 17:03:09 +0000

    Edit segmentation types

--- streamlit_app/pages/5_Segmentation.py
+++ streamlit_app/pages/5_Segmentation.py
@@ -1,16 +1,32 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from PIL import Image
 
 st.markdown("# Segmentaion")
 st.sidebar.markdown("# Segmentation")
 
-imtype = option_menu("Select Image Type", ["2D Segmentation", "3D Segmentation"], menu_icon="image",default_index=0)
-if imtype == "2D Segmentation": 
+imtype = option_menu("Select Target", ["Liver Tumor", "Spleen", "Lung"], menu_icon="image",default_index=0)
+if imtype == "Liver Tumor":
     file = st.file_uploader('Upload An Image')
     st.write("For testing purposes, you can select a sample image [here](https://www.kaggle.com/datasets/andrewmvd/medical-mnist)")
-    st.button("Rerun")
+    if file:
+        img = Image.open(file)
+        st.image(img, use_column_width=True)
+        st.button("Click Here to Segment")
 
-if imtype == "3D Segmentation":
+if imtype == "Spleen":
     file = st.file_uploader('Upload An Image')
     st.write("For testing purposes, you can select a sample image [here](https://www.kaggle.com/datasets/andrewmvd/medical-mnist)")
-    st.button("Rerun")
+    if file:
+        img = Image.open(file)
+        st.image(img, use_column_width=True)
+        st.button("Click Here to Segment")
+
+if imtype == "Lung":
+    file = st.file_uploader('Upload An Image')
+    st.write("For testing purposes, you can select a sample image [here](https://www.kaggle.com/datasets/andrewmvd/medical-mnist)")
+    if file:
+        img = Image.open(file)
+        st.image(img, use_column_width=True)
+        st.button("Click Here to Segment")
+

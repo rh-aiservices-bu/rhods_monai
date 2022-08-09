a9bd04a2b83c5d628a21531046fd7b2d2828f278
Maria Shevchuk <87496627+mariashev@users.noreply.github.com>
2022-08-09 17:07:07 -0400

    add intro

--- streamlit_app/RHODS_x_MONAI.py
+++ streamlit_app/RHODS_x_MONAI.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.markdown("# RHODS x MONAI")
 st.sidebar.markdown("# RHODS x MONAI")
+
+st.write("This application was developed, packaged, and deployed using RHODS (Red Hat OpenShift Data Science) services with the goal of showcasing the development capabilities that RHODS provides.")
+st.write("The MONAI (Medical Open Network for Artificial Intelligence) framework was implemented within the application, adding tools such as Medical Image Processing, Classification, and Segmentation.")

749b5a763563248939469d67fd336609e424beaa
Maria Shevchuk <87496627+mariashev@users.noreply.github.com>
2022-08-09 17:09:29 -0400

    Add feedback form

--- streamlit_app/RHODS_x_MONAI.py
+++ streamlit_app/RHODS_x_MONAI.py
@@ -5,3 +5,7 @@ st.sidebar.markdown("# RHODS x MONAI")
 
 st.write("This application was developed, packaged, and deployed using RHODS (Red Hat OpenShift Data Science) services with the goal of showcasing the development capabilities that RHODS provides.")
 st.write("The MONAI (Medical Open Network for Artificial Intelligence) framework was implemented within the application, adding tools such as Medical Image Processing, Classification, and Segmentation.")
+
+st.write("## Feedback")
+st.write("Please consider sharing your feedback via the form below. All suggestions are welcome and appreciated!")
+st.button("Feedback Form")

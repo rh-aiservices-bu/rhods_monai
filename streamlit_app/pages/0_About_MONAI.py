c1329180e6ad05d16956cd075e53229e1586c7a4
Maria Shevchuk <87496627+mariashev@users.noreply.github.com>
2022-08-09 16:50:46 -0400

    Add monai logo

--- streamlit_app/pages/0_About_MONAI.py
+++ streamlit_app/pages/0_About_MONAI.py
@@ -1,8 +1,13 @@
 import streamlit as st
+from PIL import Image
 
 st.markdown("# About MONAI")
 st.sidebar.markdown("# About MONAI")
+
 st.write("**(Medical Open Network for Artificial Intelligence)**")
+
+image = Image.open('../images/monai_logo.png')
+st.image(image, caption='Sunrise by the mountains')
 
 st.write("Project MONAI was originally started by NVIDIA & King’s College London to establish an inclusive community of AI researchers for the development and exchange of best practices for AI in healthcare imaging across academia and enterprise researchers.")
 

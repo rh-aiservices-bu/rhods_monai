23c5df9bb9ff291b23e616f918c2994747fa9da6
Maria Shevchuk <87496627+mariashev@users.noreply.github.com>
2022-08-09 17:35:31 -0400

    Add source code link

--- streamlit_app/RHODS_x_MONAI.py
+++ streamlit_app/RHODS_x_MONAI.py
@@ -6,6 +6,11 @@ st.sidebar.markdown("# RHODS x MONAI")
 st.write("This application was developed, packaged, and deployed using RHODS (Red Hat OpenShift Data Science) services with the goal of showcasing the development capabilities that RHODS provides.")
 st.write("The MONAI (Medical Open Network for Artificial Intelligence) framework was implemented within the application, adding tools such as Medical Image Processing, Classification, and Segmentation.")
 
-st.write("## Feedback")
+st.write("### Feedback")
 st.write("Please consider sharing your feedback via the form below. All suggestions are welcome and appreciated!")
 st.button("Feedback Form")
+
+st.write("### Source Code")
+st.write("This is an open source project. All source code and documentation can be found on GitHub.")
+if st.button("GitHub Repo"): 
+  webbrowser.open_new_tab(https://github.com/rh-aiservices-bu/rhods_monai.git)
